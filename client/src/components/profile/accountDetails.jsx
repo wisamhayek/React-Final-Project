207841203f5775d0a9bbdfed3056b2931310f7c2
Wisam Hayek <hayekwisam@gmail.com>
2022-07-26 14:25:03 -0700

    Profile tabs layout

--- client/src/components/profile/accountDetails.jsx
+++ client/src/components/profile/accountDetails.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import React, { Fragment } from 'react'
 
 export default function AccountDetails() {
   return (
-    <div>Account Details</div>
+    <Fragment>
+    <Typography variant='h5' sx={{margin:"auto",textAlign:"center", marginBottom:"2rem"}}>Account Details</Typography>
+    <Box sx={{margin:"auto",textAlign:"center", width:"fit-content"}}>
+      <div style={{display:"grid",gridTemplateColumns: "1fr 1fr"}}>
+        <div className='textFields'>
+          <Typography variant='h6'>Name : </Typography>
+          <Typography variant='h6'>Email :</Typography>
+          <Typography variant='h6'>Password :</Typography>
+        </div>
+        <div className='rightFields'>
+          <Typography variant='h6'>Take from Global Context</Typography>
+          <Typography variant='h6'>Context :</Typography>
+          <Typography variant='h6'>Context :</Typography>
+        </div>
+      </div>
+    </Box>
+    </Fragment>
   )
 }
+
+// Render if Mobile
+// https://stackoverflow.com/questions/44480053/how-to-detect-if-screen-size-has-changed-to-mobile-in-react
--- client/src/components/profile/orderHistory.jsx
+++ client/src/components/profile/orderHistory.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import { borderTop } from '@mui/system'
+import React, { Fragment } from 'react'
 
 export default function OrderHistory() {
   return (
-    <div>Order History</div>
+    <Fragment>
+    <Typography variant='h5' sx={{margin:"auto",textAlign:"center",marginBottom: "2rem"}}>Order History</Typography>
+
+    {/* Desktop View */}
+    <Box sx={{margin:"auto",textAlign:"center",display: {xs: "none", md: "grid"},gridTemplateColumns:" 40% 60%", borderTop:"1px solid gray", borderBottom:"1px solid gray"}}>
+      <div>
+        <img src="#" alt="image"/>
+      </div>
+      <div style={{display:"grid"}}>
+        <div className='textFields'>
+          <Typography variant='h6'>Order Number : XXXX</Typography>
+          <Typography variant='h6'>Total Price : $$$</Typography>
+          <Typography variant='h6'>Date : 11/11/2021</Typography>
+          <Typography variant='h6'>Status : Pending</Typography>
+        </div>
+        <div className='buttons'>
+          <Button>Track</Button>
+          <Button>Cancel</Button>
+          <Button>Invoice</Button>
+        </div>
+      </div>
+    </Box>
+
+    {/* Mobile View */}
+    <Box sx={{margin:"auto",textAlign:"start",display: {xs: "grid", md: "none"}, borderTop:"1px solid gray", borderBottom:"1px solid gray"}}>
+      <div style={{display:"grid"}}>
+        <Typography variant='h6'>Order Number : XXXX</Typography>
+        <Typography variant='h6'>Total Price : $$$</Typography>
+        <Typography variant='h6'>Date : 11/11/2021</Typography>
+        <Typography variant='h6'>Status : Pending</Typography>
+      </div>
+
+    <div style={{display:"grid",gridTemplateColumns:"1fr 1fr"}}>
+      <div>
+        <img src="#" alt="product" style={{width:"200px",height:"200px"}}/>
+      </div>
+      <div className='buttons' style={{display:"grid"}} >
+        <Button>Track</Button>
+        <Button>Cancel</Button>
+        <Button>Invoice</Button>
+      </div>
+    </div>
+    </Box>
+    </Fragment>
   )
 }
